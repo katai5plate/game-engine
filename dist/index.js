fd5ff132fa2755a607a398a8369276c07865d94b
katai5plate <katai5plate.sub@gmail.com>
2022-06-04 01:09:44 +0900

    npm-scriptsを調整

--- dist/index.js
+++ dist/index.js
@@ -51284,7 +51284,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "2222" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "3179" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
